05be989827a60f9ddb88522aa01fd6f188fc2f68
Bosco Choi <bchoi@lcs.on.ca>
2020-10-23 11:05:28 -0400

    Finished Magic Square

--- MagicSquares/main.swift
+++ MagicSquares/main.swift
@@ -7,72 +7,57 @@
 //  https://www.russellgordon.ca/incomplete-puzzles/magic-squares.pdf
 //
 //  NOTE: Puzzle adapted from material provided by the University of Waterloo.
-
 import Foundation
 
 print("Magic Squares")
 print("=============")
 
 // INPUT
-
 // Create an empty array to store each row of numbers
 // NOTE: Each row will itself be an array, effectively creating a two-dimensional array.
 var numbers: [[String]] = []
+var ismagicsquare:Bool = false
 
 // Ask for the first row of numbers
 // NOTE: This is provided as a String
-let line1 = readLine()!
 
-// Now split the first row of numbers using the space character as a delimiter, and then place the resulting array in the 2D array of numbers
-// NOTE: Set a breakpoint and use the debugger to see what the "numbers" array contains at this point
-numbers.append(line1.components(separatedBy: " "))
+// Asking for user input really cool and interesting
 
-// Ask for the second row of numbers
-// NOTE: This is provided as a string
-let line2 = readLine()!
+for _ in 0...3{
+    let line1 = readLine()!
+    numbers.append(line1.components(separatedBy: " "))
+}
 
-// Now split the first row of numbers using the space character as a delimiter, and then place the resulting array in the 2D array of numbers
-// NOTE: Set a breakpoint and use the debugger to see what the "numbers" array contains at this point
-numbers.append(line2.components(separatedBy: " "))
+// This would print hte row and stuff maakeing sure everthing works
+// This is for the look of thing
 
-// PROCESS & OUTPUT
-
-// Print each row and column of the array
-for (i, row) in numbers.enumerated() {
-    for (j, column) in row.enumerated() {
-        // NOTE: At this point, each value is still a string. What must you do prior to adding up the values? 🧐
-        // HINT: Think data types.
-        print("The value at row \(i) and column \(j) is: \(column)")
+for i in 0...3{
+    var count:Int = 0
+    for x in 0...3{
+        count += Int(numbers[i][x]) ?? 0
+    }
+    if count != 34{
+        ismagicsquare = false
     }
 }
 
-// Print each row and column of the array, showing values in a formatted manner
-// Iterate over the rows
-for row in 0...numbers.count - 1 {
+// This would make things look nice really cool and all
+// This is the function of things.
 
-    // Print gap (a tab character) in top-left corner and headers for columns
-    if row == 0 {
-        print("\t", terminator: "")
-        for column in 0...numbers[row].count - 1 {
-            print("\(column)\t", terminator: "")
-        }
-        print("") // Go to next line
+for j in 0...3{
+    var av:Int = 0
+    
+    for x in 0...3{
+        av += Int(numbers[x][j]) ?? 0
     }
     
-    // Iterate over the columns
-    for column in 0...numbers[row].count - 1 {
-        
-        // Print row header
-        if column == 0 {
-            print("\(row)\t", terminator: "")
-        }
-        
-        // Print the actual number at this row and column
-        print("\(numbers[row][column])\t", terminator: "")
-        
+    if av != 34{
+        ismagicsquare = false
     }
-    
-    // Now go to the next line
-    print("")
-    
+}
+
+if ismagicsquare{
+    print("Magic Square")
+}else{
+    print("Not Magic Square")
 }
